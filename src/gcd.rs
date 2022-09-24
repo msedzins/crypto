3d41e89bdd9f85f18bb41d5c2f1e752cbeb4986d
msedzins <msedzins@gmail.com>
2022-09-25 01:05:48 +0200

    GCD switched to generics

--- src/gcd.rs
+++ src/gcd.rs
@@ -1,6 +1,9 @@
-pub fn calculate(x: u32, y: u32) -> u32 {
+pub fn calculate<T>(x: T, y: T) -> T
+where
+    T: Default + std::cmp::PartialEq + std::ops::Rem<Output = T> + Copy
+{
     let reminder = x % y;
-    if reminder == 0 {
+    if reminder == T::default() {
         return y;
     }
     return calculate(y, reminder);
@@ -11,8 +14,32 @@ mod tests {
     use super::*;
 
     #[test]
-    fn calculate_gcd() {
-        let x = calculate(2024, 748);
+    fn calculate_gcd_u32() {
+        
+        let x: u32  = 2024;
+        let y: u32 = 748;
+        let x: u32 = calculate(x, y);
+        
         assert_eq!(x, 44);
+    }
+
+    #[test]
+    fn calculate_gcd_i32() {
+        
+        let x: i32  = 2024;
+        let y: i32 = 748;
+        let x: i32 = calculate(x, y);
+        
+        assert_eq!(x, 44);
+    }
+
+    #[test]
+    fn calculate_gcd_i8() {
+        
+        let x: i8  = 120;
+        let y: i8 = 14;
+        let x: i8 = calculate(x, y);
+        
+        assert_eq!(x, 2);
     }
 }
